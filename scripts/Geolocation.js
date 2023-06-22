ba283c15cf31260c89d79b0eebc5850ed30c9310
rafaeusilvar22 <rafaelsilvar221@gmail.com>
2023-06-22 13:20:37 -0300

    first commit

---
+++ scripts/Geolocation.js
@@ -0,0 +1,133 @@
+window.onload = () => {
+  let button = document.querySelector('button[data-action="change"]');
+  let btnAbout = document.querySelector('button[data-action="teste"]');
+  btnAbout.innerText = "?";
+  button.innerHTML =
+    '<img width="40px" height="40px" src="assets/images/return.png"/>';
+
+  let places = staticLoadPlaces();
+  renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+  return [
+    {
+      name: "Animaismon",
+      location: {
+        lat: -26.915493045742146,
+        lng: -49.05746340751648,
+      },
+    },
+  ];
+}
+
+let models = [
+  {
+    url: "assets/low_poly_dog/scene.gltf",
+    scale: "0.001 0.001 0.001",
+    rotation: "0 180 0",
+    info: "Cachorro Caramelo, Lv. 10, HP 150/150",
+    about:
+      "O vira-lata caramelo, é um cão sem raça definida que se popularizou por ser encontrado com facilidade pelo Brasil. É considerado pelas pessoas um patrimônio nacional.",
+  },
+  {
+    url: "assets/dollynho_dance/scene.gltf",
+    scale: "0.5 0.5 0.5",
+    rotation: "0 180 0",
+    info: "Refrigerante Dolly, Lv. 25, HP 40/40",
+    about:
+    "Oi pessoal, eu sou o Dollynho, seu amiguinho! Segundo o meu proprietário, fui inspirado no Teletubbies."
+  },
+  {
+    url: "assets/loro_jose/scene.gltf",
+    scale: "0.01 0.01 0.01",
+    rotation: "0 180 0",
+    info: "Loro Jose, Lv. 10, HP 10/10",
+    about:
+    "Louro José foi um boneco de um papagaio que misturava artifícios de fantoches com a tecnologia de controle remoto."
+  },
+  {
+    url: "assets/lesser_dog_-_undertale/scene.gltf",
+    scale: "0.005 0.005 0.005",
+    rotation: "0 180 0",
+    info: "Cachorro Guerreiro, Lv. 70, HP 150/150",
+    about:
+    "Um cachorro equipado com uma espada muito afiada e um escudo extremamente resistente"
+  },
+  {
+    url: "assets/capybara/scene.gltf",
+    scale: "0.5 0.5 0.5",
+    info: "Capivara, Lv. 10, HP 30/10",
+    rotation: "0 180 0",
+    about:
+    "A capivara destaca-se por ser o maior roedor do mundo. É um animal herbívoro que pode ser encontrado nas Américas Central e do Sul."
+  },
+  {
+    url: "assets/samurai_capybara/scene.gltf",
+    scale: "0.9 0.9 0.9",
+    rotation: "0 180 0",
+    info: "Capivara Samurai, Lv. 80, HP 100/100",
+    about:
+    "Cansei de ser um Templário Ardente, agora farei jus ao meu nome e serei a Capivara Samurai mais forte de Blumenau"
+  },
+];
+
+var modelIndex = 0;
+var setModel = function (model, entity) {
+  if (model.scale) {
+    entity.setAttribute("scale", model.scale);
+  }
+
+  if (model.rotation) {
+    entity.setAttribute("rotation", model.rotation);
+  }
+
+  if (model.position) {
+    entity.setAttribute("position", model.position);
+  }
+
+  entity.setAttribute("gltf-model", model.url);
+
+  const div = document.querySelector(".instructions");
+  let about = document.querySelector(".about");
+  about.innerHTML = `<p>${model.about}</p>`;
+  div.innerText = model.info;
+
+  document
+    .querySelector('button[data-action="teste"]')
+    .addEventListener("click", () => {
+      about.classList.remove("hidden");
+    });
+};
+
+function renderPlaces(places) {
+  let scene = document.querySelector("a-scene");
+
+  places.forEach((place) => {
+    let latitude = place.location.lat;
+    let longitude = place.location.lng;
+
+    let model = document.createElement("a-entity");
+    model.setAttribute(
+      "gps-entity-place",
+      `latitude: ${latitude}; longitude: ${longitude};`
+    );
+    model.setAttribute("animation-mixer", "");
+
+    setModel(models[modelIndex], model);
+
+    model.setAttribute("class", "clickable");
+
+    document
+      .querySelector('button[data-action="change"]')
+      .addEventListener("click", () => {
+        let entity = document.querySelector("[gps-entity-place]");
+        modelIndex++;
+        let newIndex = modelIndex % models.length;
+        setModel(models[newIndex], entity);
+        let about = document.querySelector(".about");
+        about.classList.add("hidden");
+      });
+    scene.appendChild(model);
+  });
+}
---
+++ scripts/script.js
@@ -0,0 +1,74 @@
+// let btnLinkdin = document.querySelector("#linkedin");
+
+// btnLinkdin.addEventListener("click", () =>{
+//     window.open("https://www.linkedin.com/in/rafael-silva-480a67222/", '_blank');
+
+// });
+
+let BtnLogoWind = document.querySelector("#BtnLogoWind");
+let attributeWind = document.querySelector("#attributeWind");
+
+function clear() {
+  attributeWind.setAttribute("visible", "false");
+  BtnLogoWind.setAttribute(
+    "animation",
+    "property: scale; to: 0.01.5 0.01.5 0.01.5; easing: easeOutQuad; loop: true; dir: alternate"
+  );
+  BtnIconLevel.setAttribute(
+    "animation",
+    "property: scale; to: 0.01.5 0.01.5 0.01.5; easing: easeOutQuad; loop: true; dir: alternate"
+  );
+  attributeWind.removeAttribute('value');
+}
+
+BtnLogoWind.addEventListener("click", () => {
+  attributeWind.setAttribute("visible", "true");
+  attributeWind.setAttribute('value', "Monstros do atributo VENTO geralmente sao monstros voadores. Como Fera-alada,  Inseto voador, Fada e Dragao.")
+  BtnLogoWind.removeAttribute("animation");
+  setTimeout(clear, 9000);
+});
+
+
+let BtnIconLevel = document.querySelector('#BtnIconLevel');
+
+BtnIconLevel.addEventListener('click', () =>{
+  attributeWind.setAttribute('visible', 'true');
+  attributeWind.setAttribute('value', 'O Nivel de um Card de Monstro, representado por estrelas no lado superior direito do card, geralmente mostra o quao poderoso e valioso o monstro. O Nivel minimo 1, e o maximo 12.')
+  BtnIconLevel.removeAttribute("animation");
+  setTimeout(clear, 9000);
+})
+
+let btnInformation = document.querySelector("#btnInformation");
+let textAbout = document.querySelector("#about");
+let btnStorePharmacy = document.querySelector("#storePharmacy");
+let textStorePharmacy = document.querySelector("#textStorePharmacy");
+let aboutNext = document.querySelector("#aboutNext");
+let btnNextAbout = document.querySelector("#btnNextAbout");
+let btnReturnAbout = document.querySelector("#btnReturnAbout");
+
+btnInformation.addEventListener("click", () => {
+  console.log("clicou");
+  textAbout.setAttribute(
+    "value",
+    "Paracetamol é um analgesico e antipiretico, sendo indicado para a alivio da dor de intensidade leve a moderada, incluindo dor de cabeca, enxaqueca, dor musculo esqueletica, colicas menstruais, dor de garganta, dor de dente, dor pos-procedimentos odontologicos, dor e febre apos vacinacao, e dor de osteoartrite."
+  );
+  btnStorePharmacy.setAttribute("visible", "true");
+  textStorePharmacy.setAttribute("visible", "true");
+  btnNextAbout.setAttribute("visible", "true");
+  btnInformation.removeAttribute("animation");
+});
+
+btnStorePharmacy.addEventListener("click", () => {
+  window.open(
+    "https://www.panvel.com/panvel/buscarProduto.do?termoPesquisa=paracetamol",
+    "_blank"
+  );
+});
+
+btnNextAbout.addEventListener("click", () => {
+  textAbout.setAttribute(
+    "value",
+    "Este medicamento nao deve ser usado em caso de hipersensibilidade ao paracetamol ou a qualquer outro componente da formula."
+  );
+  btnNextAbout.setAttribute("visible", "false");
+});
